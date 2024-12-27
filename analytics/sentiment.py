069766ba8042144fe87b148eda4876e07d203531
Harshal Gunjal <111036474+HarshalGunjalOp@users.noreply.github.com>
2024-12-27 05:13:06 +0000

    added sentiment analysis

---
+++ analytics/sentiment.py
@@ -0,0 +1,51 @@
+import sqlite3
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+# Connect to the database
+conn = sqlite3.connect('../db.sqlite')
+cursor = conn.cursor()
+
+# Retrieve comments
+cursor.execute("SELECT id, comment FROM comments")
+rows = cursor.fetchall()
+
+sia = SentimentIntensityAnalyzer()
+
+sentiment_results = []
+
+for row in rows:
+    comment_id = row[0]
+    comment_text = row[1]
+    
+    # Run VADER analysis
+    scores = sia.polarity_scores(comment_text)
+    
+    # For example, you might categorize sentiment by the compound score:
+    compound_score = scores['compound']
+    if compound_score >= 0.05:
+        sentiment_label = 'positive'
+    elif compound_score <= -0.05:
+        sentiment_label = 'negative'
+    else:
+        sentiment_label = 'neutral'
+    
+    sentiment_results.append((comment_id, sentiment_label, compound_score))
+
+conn.close()
+
+
+total=0
+for i in sentiment_results:
+    total+= i[2]
+
+average=total/(len(sentiment_results))
+
+if average >= 0.05:
+    print('positive')
+elif average <= -0.05:
+    print('negative')
+else:
+    print('neutral')
--- main.py
+++ main.py
@@ -1,3 +1,43 @@
+import praw
+from prawcore.exceptions import ResponseException
+import time
+import datetime
 
+# Initialize the Reddit client
+reddit = praw.Reddit(
+        client_id='NNTlbq_GJlrUXwOCw5TC2Q',
+        client_secret='vNcIczNFXXpIe0G2WEPfWdpL9SJt0Q',
+        user_agent='realtime-analysis-and-prediction-dashboard-for-social-media-data'
+)
+
+def search_keyword_in_comments_of_latest_posts(keyword, subreddit_name='all', limit=100):
+    """
+    Searches for a given keyword in the comments of the latest posts in reddit.
+
+    :param keyword: The keyword to search for.
+    :param limit: How many of the latest submissions to analyze (default=10).
+    :return: A list of comments that contain the keyword.
+    """
+    comments_with_keyword = []
+    for comment in reddit.subreddit(subreddit_name).comments(limit=limit):
+        if keyword in comment.body:
+            comments_with_keyword.append(comment)
+    return comments_with_keyword
+
+if __name__ == "__main__":
+    start = time.time()
+    
+    keyword = input("Enter a keyword to search for: ")
+    subreddit_name = input("Enter the subreddit name (default='all'): ")
+    comments_with_keyword = search_keyword_in_comments_of_latest_posts(keyword, subreddit_name, 1000)
+    if comments_with_keyword:
+        print(f"Comments containing '{keyword}':", len(comments_with_keyword))
+    else:
+        print(f"No comments containing '{keyword}' found.")
+        
+    end = time.time()
+    print(f"Execution time: {end - start} seconds")
+    
+    
 
     
